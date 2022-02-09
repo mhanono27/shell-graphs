8f45902740cba2a3ed19f4f0a128cca4f748de86
mhanono27 <98059885+mhanono27@users.noreply.github.com>
2022-02-09 15:26:14 -0500

    Delete src directory

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-
-// 30% DAI 30% USDC 30% USDT 10% SUSD 
-export const SHELL: string = "0x8f26D7bAB7a73309141A291525C965EcdEa7Bf42"
-// 50% WBTC 40% RENBTC 10% SBTC 
-// export const SHELL: string = "0xC2D019b901f8D4fdb2B9a65b5d226Ad88c66EE8D"
-
-export const ONE: BigInt = BigInt.fromI32(1)
-export const ZERO_ADDR: string = "0x0000000000000000000000000000000000000000"
